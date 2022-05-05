35bfd90a23b1feaf0ecc332360fb91bec19da451
mdsafiulazam <mdriadkhan20@gmail.com>
2022-05-05 10:42:52 +0600

    added google signin and github signin with react firebase hooks

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,8 +2,14 @@ import React from 'react';
 import google from '../../../images/social-icon/google.png';
 import github from '../../../images/social-icon/github.png';
 import './SocialLogin.css'
+import { useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const SocialLogin = () => {
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithGithub, gitUser, gitLoading, gitError] = useSignInWithGithub(auth);
+
+   
     return (
         <div>
             <div className='d-flex align-items-center'>
@@ -12,11 +18,11 @@ const SocialLogin = () => {
                 <div style={{ height: '1px' }} className='line-color w-50'></div>
             </div>
             <div>
-                <button className='btn social-btn w-50 mx-auto d-block my-3'>
+                <button onClick={()=>signInWithGoogle()} className='btn social-btn w-50 mx-auto d-block my-3'>
                     <img className='me-2' src={google} alt="" />
                      Google Sign In
                     </button>
-                <button className='btn btn-dark social-btn w-50 mx-auto d-block my-3'>
+                <button onClick={()=>signInWithGithub()} className='btn btn-dark social-btn w-50 mx-auto d-block my-3'>
                     <img className='me-2' src={github} alt="" />
                      GitHub Sign In
                     </button>
