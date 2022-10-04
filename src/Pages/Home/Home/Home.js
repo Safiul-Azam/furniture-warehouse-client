8e8e0f3c07551d75bbe4b7d5d7eb025d305a6f78
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-04 19:38:13 +0600

    added sliders component in home

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,8 +3,9 @@ import InventoryItem from "../Inventory/InventoryItem/InventoryItem";
 import NewArrivals from "../NewArrivals/NewArrivals";
 import OurBlogs from "../OurBlogs/OurBlogs/OurBlogs";
 import "./Home.css";
-import img from '../../../images/banner/banner1.png'
+import img from "../../../images/banner/banner1.png";
 import { Link } from "react-router-dom";
+import Sliders from "../Sliders/Sliders";
 
 const Home = () => {
   return (
@@ -15,20 +16,24 @@ const Home = () => {
           background: `linear-gradient(rgba(0, 0, 0, 0.679),rgba(0, 0, 0, 0.647)),url(${img})`,
           backgroundPosition: "center",
           backgroundSize: "cover",
-          paddingTop:"200px",
-          paddingBottom:'200px'
+          paddingTop: "200px",
+          backgroundRepeat:'no-repeat',
+          paddingBottom: "200px",
         }}
       >
         <div className="container h-100">
-          <h3 className="text-center banner-title">
+          <h3 className="text-center banner-title mb-5">
             JACOB Dining Hansford-193 and Jellyroll-193 (6 Chair and 1
             Table)DINING TABLE
           </h3>
-          <Link to='/allInventory' className="update-btn d-block mx-auto">Update</Link>
+          <div className="update-btn text-center">
+            <Link to="/allInventory">All Inventories</Link>
+          </div>
         </div>
       </div>
       <InventoryItem></InventoryItem>
       <NewArrivals></NewArrivals>
+      <Sliders/>
       <OurBlogs></OurBlogs>
     </div>
   );
--- src/Pages/Home/Sliders/Sliders.js
+++ src/Pages/Home/Sliders/Sliders.js
@@ -5,11 +5,30 @@ import { Autoplay, Pagination } from "swiper";
 import slider1 from "../../../images/slider/slider1.jpg";
 import slider2 from "../../../images/slider/slider2.jpg";
 import slider3 from "../../../images/slider/slider3.jpg";
+import { Link } from "react-router-dom";
 const banners = [
-    { id: 1, img: slider1, title:'The Salamander is a sofa, bed, and discreet storage space in one.Whether you’re lounging with family during the weekends or accommodating guests overnight, the Salamander provides you with great convenience by virtue of its versatility.', subTitle:'Sofa Salamander-244' },
-    { id: 2, img: slider2, title:'Made from Kiln-dried imported Beech veneered engineered wood High quality environment friendly Italian Ultra Violet (UV) and Polyurethane(PU) Imported fabric upholstery with soft and durable cushioning. Fabric can be selected from available options', subTitle:'Sofa Morris-287' },
-    { id: 3, img: slider3, title:'Made from Kiln-dried imported Beech wood and veneered engineered wood and veneered MDF High quality environment friendly ItalianUltra Violet (UV) and Polyurethan(PU) Lacquer in antique finish Please refer to images for dimension details Imported fabric upholstery with soft and durable cushioning', subTitle:'Sofa Angelina-179' },
-  ]
+  {
+    id: 1,
+    img: slider1,
+    title:
+      "The Salamander is a sofa, bed, and discreet storage space in one.Whether you’re lounging with family during the weekends or accommodating guests overnight, the Salamander provides you with great convenience by virtue of its versatility.",
+    subTitle: "Sofa Salamander-244",
+  },
+  {
+    id: 2,
+    img: slider2,
+    title:
+      "Made from Kiln-dried imported Beech veneered engineered wood High quality environment friendly Italian Ultra Violet (UV) and Polyurethane(PU) Imported fabric upholstery with soft and durable cushioning. Fabric can be selected from available options",
+    subTitle: "Sofa Morris-287",
+  },
+  {
+    id: 3,
+    img: slider3,
+    title:
+      "Made from Kiln-dried imported Beech wood and veneered engineered wood and veneered MDF High quality environment friendly ItalianUltra Violet (UV) and Polyurethan(PU) Lacquer in antique finish Please refer to images for dimension details Imported fabric upholstery with soft and durable cushioning",
+    subTitle: "Sofa Angelina-179",
+  },
+];
 
 const Sliders = () => {
   return (
@@ -19,7 +38,7 @@ const Sliders = () => {
         slidesPerView={1}
         slidesPerGroup={1}
         autoplay={{
-          delay: 3000,
+          delay: 3500,
           disableOnInteraction: false,
         }}
         loop={true}
@@ -27,29 +46,31 @@ const Sliders = () => {
         pagination={{
           clickable: true,
         }}
-        modules={[Autoplay, Pagination]}
-        className="mySwiper h-screen"
+        modules={[Autoplay, Pagination,]}
+        className="mySwiper"
       >
         {banners.map((banner) => (
           <SwiperSlide
             key={banner.id}
-            className="pt-6"
             style={{
-              background: `linear-gradient(rgb(0,0,0,0.3),rgb(0,0,0,0.3)),url(${banner.img})`,
+              background: `linear-gradient(rgba(0, 0, 0, 0.679),rgba(0, 0, 0, 0.647)),url(${banner.img})`,
               backgroundPosition: "center",
+              backgroundRepeat:'no-repeat',
+              paddingTop: "200px",
               backgroundSize: "cover",
+              paddingBottom: "200px",
             }}
           >
-            <div className="text-white text-center mt-44 lg:mt-40">
-              <p className="lg:text-lg md:text-lg text-sm uppercase mb-6 lg:tracking-[10px] tracking-[4px]">
+            <div className="text-white w-75 mx-auto text-center">
+              <p className="fs-6">
                 {banner.subTitle}
               </p>
-              <h2 className="lg:text-6xl md:text-4xl text-3xl uppercase lg:w-3/4 w-11/12 mx-auto mb-7 lg:tracking-[20px] lg:leading-relaxed md:leading-relaxed sm:leading-relaxed tracking-[8px]">
+              <h2 className="fs-4 my-4">
                 {banner.title}
               </h2>
-              <button className="py-4 px-5 bg-transparent hover:bg-black rounded-none text-white hover:border-black hover:duration-500 lg:tracking-[5px] tracking-widest border hover:ease-in-out ease-in-out duration-500 uppercase">
-                Rooms & Suites
-              </button>
+              <div className="update-btn text-center">
+                <Link to="/allInventory">All Inventories</Link>
+              </div>
             </div>
           </SwiperSlide>
         ))}
