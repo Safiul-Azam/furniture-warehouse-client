ed722e8e5a6b83fb2ff2bd0a1dbb5bd70dbfa010
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-04 20:26:58 +0600

    all is up- to-date

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -14,13 +14,13 @@ const Header = () => {
   return (
     <div className="navbar sticky-top bg-white shadow-lg">
       <div className="container d-flex align-items-center justify-content-between">
-        <h2 className="fs-6">
+        <h2 className="fs-6 my-4">
           FURNITURE <span className="title-color">WARE HOUSE</span>
         </h2>
         <Navbar collapseOnSelect expand="sm">
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="d-block mx-auto navbar-menu fs-5">
+            <Nav className="d-block mx-auto navbar-menu fs-6">
               <Link to="/home">Home</Link>
               <Link to="/Our-blogs">Our Blogs</Link>
               <Link to="/blogs">Question & Answer</Link>
