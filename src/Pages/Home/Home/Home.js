027033f109f75ec02d20441765c778e5eef91c13
Safiul-Azam <safiulazamriad@gmail.com>
2022-10-04 19:12:22 +0600

    update home , header component 4/10/22

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,23 +1,37 @@
-import React from 'react';
-import InventoryItem from '../Inventory/InventoryItem/InventoryItem';
-import NewArrivals from '../NewArrivals/NewArrivals';
-import OurBlogs from '../OurBlogs/OurBlogs/OurBlogs';
-import './Home.css'
+import React from "react";
+import InventoryItem from "../Inventory/InventoryItem/InventoryItem";
+import NewArrivals from "../NewArrivals/NewArrivals";
+import OurBlogs from "../OurBlogs/OurBlogs/OurBlogs";
+import "./Home.css";
+import img from '../../../images/banner/banner1.png'
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div>
-            <div className='banner'>
-                <div className='w-100 container'>
-                    <h3 className='text-center banner-title'>JACOB Dining Hansford-193 and Jellyroll-193 (6 Chair and 1 Table)DINING TABLE</h3>
-                    <button className='update-btn d-block mx-auto'>Update</button>
-                </div>
-            </div>
-            <InventoryItem></InventoryItem>
-            <NewArrivals></NewArrivals>
-            <OurBlogs></OurBlogs>
+  return (
+    <div>
+      <div
+        className=""
+        style={{
+          background: `linear-gradient(rgba(0, 0, 0, 0.679),rgba(0, 0, 0, 0.647)),url(${img})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          paddingTop:"200px",
+          paddingBottom:'200px'
+        }}
+      >
+        <div className="container h-100">
+          <h3 className="text-center banner-title">
+            JACOB Dining Hansford-193 and Jellyroll-193 (6 Chair and 1
+            Table)DINING TABLE
+          </h3>
+          <Link to='/allInventory' className="update-btn d-block mx-auto">Update</Link>
         </div>
-    );
+      </div>
+      <InventoryItem></InventoryItem>
+      <NewArrivals></NewArrivals>
+      <OurBlogs></OurBlogs>
+    </div>
+  );
 };
 
 export default Home;
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,51 +1,50 @@
-import { signOut } from 'firebase/auth';
-import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { Link } from 'react-router-dom';
-import auth from '../../../firebase.init';
-import './Header.css'
+import { signOut } from "firebase/auth";
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../../firebase.init";
+import "./Header.css";
 
 const Header = () => {
-    const [user] = useAuthState(auth)
-    const handleSignOut = () => {
-        signOut(auth)
-    }
-    return (
-        <div className='navbar sticky-top'>
-            <div className='container'>
-                <h2 className='logo'>FURNITURE <span className='title-color'>WARE HOUSE</span></h2>
-                <div>
-                    <Navbar collapseOnSelect expand="lg">
-                        <Container>
-                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                            <Navbar.Collapse id="responsive-navbar-nav">
-                                <Nav className='w-70 d-block mx-auto navbar-menu' >
-                                    <Link to='/home'>Home</Link>
-                                    <Link to='/Our-blogs'>Our Blogs</Link>
-                                    <Link to='/blogs'>Question & Answer</Link>
-                                    <Link to='/allInventory'>All-Inventory</Link>
-                                    {
-                                        user && <>
-                                            <Link to='/addItem'>Add Item</Link>
-                                            <Link to='/manage-inventory'>Manage-Inventory</Link>
-                                            <Link to='/myAddedProducts'>My-item</Link>
-                                        </>
-                                    }
-                                    {user ?
-                                        <Link onClick={() => handleSignOut()} to=''>SignOut</Link> :
-                                        <Link to='login'>Login</Link>
-                                    }
-                                </Nav>
-                                <Nav>
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
-                </div>
-            </div>
-        </div>
-    );
+  const [user] = useAuthState(auth);
+  const handleSignOut = () => {
+    signOut(auth);
+  };
+  return (
+    <div className="navbar sticky-top bg-white shadow-lg">
+      <div className="container d-flex align-items-center justify-content-between">
+        <h2 className="fs-6">
+          FURNITURE <span className="title-color">WARE HOUSE</span>
+        </h2>
+        <Navbar collapseOnSelect expand="sm">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="d-block mx-auto navbar-menu fs-5">
+              <Link to="/home">Home</Link>
+              <Link to="/Our-blogs">Our Blogs</Link>
+              <Link to="/blogs">Question & Answer</Link>
+              <Link to="/allInventory">All-Inventory</Link>
+              {user && (
+                <>
+                  <Link to="/addItem">Add Item</Link>
+                  <Link to="/manage-inventory">Manage-Inventory</Link>
+                  <Link to="/myAddedProducts">My-item</Link>
+                </>
+              )}
+              {user ? (
+                <Link onClick={() => handleSignOut()} to="">
+                  SignOut
+                </Link>
+              ) : (
+                <Link to="login">Login</Link>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
